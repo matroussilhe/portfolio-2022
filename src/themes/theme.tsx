c32135cbebf73c17efc2640da667c15afbe67343
matroussilhe <mathieu.roussilhe@gmail.com>
2022-01-17 17:58:53 +0900

    Fix font familly quotes

--- src/themes/theme.tsx
+++ src/themes/theme.tsx
@@ -155,8 +155,8 @@ export const theme = getTheme({
   },
   fonts: {
     heading: "Signifier, serif",
-    body: "Scto Grotesk A, sans-serif",
-    label: "Scto Grotesk A, sans-serif",
+    body: "\"Scto Grotesk A\", sans-serif",
+    label: "\"Scto Grotesk A\", sans-serif",
   },
   fontSizes: {
     heading1: "8.75rem", //  140 px
