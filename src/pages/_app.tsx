a410ac128cb81854bf529ed574c5d0da30e459d3
matroussilhe <mathieu.roussilhe@gmail.com>
2022-02-24 17:14:12 +0900

    Update title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Fragment>
       <Head>
-        <title>{"Mat Roussilhe | Fullstack Engineer"}</title>
+        <title>{"Mathieu Roussilhe | Fullstack Engineer"}</title>
         <meta name={"description"} content={"Mathieu Roussilhe's portfolio"}/>
         <link rel={"icon"} href={"/icons/favicon.ico"}/>
       </Head>
