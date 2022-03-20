db8abbf4ba17defe690a1b2667374454dd34358d
matroussilhe <mathieu.roussilhe@gmail.com>
2022-03-20 10:27:53 +0900

    Cleanup photo section

--- src/components/organisms/section-photo.tsx
+++ src/components/organisms/section-photo.tsx
@@ -2,6 +2,7 @@ import React, {
   FunctionComponent,
   useCallback,
   useEffect,
+  useMemo,
   useRef,
 } from "react";
 
@@ -53,8 +54,8 @@ const RANDOM_X_SKIP_MAX = 2;
 const RANDOM_OFFSET_X_BASE = 0.75;
 const RANDOM_OFFSET_Y_BASE = 0.5;
 const OVERLAP_LIMIT = 0.4;
-const maxWheel = 100;
-const autoSpeed = -1;
+const MAX_WHEEL = 100;
+const AUTO_SPEED = -1;
 
 const lerp = (x1: number, x2: number, t: number) => {
   return x1 + (x2 - x1) * t;
@@ -76,42 +77,187 @@ export const SectionPhoto: FunctionComponent<SectionPhotoProps> = ({
   photo,
   ...rest
 }) => {
+  // get window dimensions
   const {
     width: windowWidth,
     height: windowHeight,
   } = useResponsiveContext();
 
-  // TODO: move outside or use useMemo
-  // get random background color
-  const backgroundColors = ["red-700", "green-700", "blue-700"];
-  const index = getRandomInteger(0, backgroundColors.length - 1);
-  const backgroundColor = backgroundColors[index];
-
+  // element refs
   const ref = useRef<HTMLDivElement>(document.createElement("div"));
   const gridItems = useRef<SectionPhotoGridItem[]>([]);
-  const totalHeight = useRef(0);
 
+  // calculation refs
+  const totalHeight = useRef(0);
   const target = useRef(0);
   const targetPercent = useRef(0);
+  const oldTarget = useRef(0);
+  const lerpedAutoScroll = useRef(1);
+  const autoScroll = useRef(1);
+  const timeAutoScroll = useRef(1);
+  const fakeTimer = useRef(0);
+  const requestAnimationFrame = useRef<number>();
 
-  let oldTarget = 0;
-  let requestAnimationFrame: number;
-  let lerpedAutoScroll = 1;
-  let autoScroll = 1;
-  let timeAutoScroll = 1;
-  let fakeTimer = 0;
+  const updateTarget = useCallback((event: WheelEvent) => {
+    let deltaWheelY = 0;
+    if (event.deltaY > 0) {
+      deltaWheelY = Math.min(event.deltaY, MAX_WHEEL);
+    } else {
+      deltaWheelY = -Math.min(Math.abs(event.deltaY), MAX_WHEEL);
+    }
 
-  const initScroll = () => {
-    const onWheel = (event: WheelEvent) => {
-      event.preventDefault();
+    target.current -= deltaWheelY;
+    target.current = Math.round(target.current * 10000) / 10000;
+    targetPercent.current = (target.current / totalHeight.current) % 1;
 
-      updateTarget(event);
-    };
+    autoScroll.current = 0;
+    timeAutoScroll.current = 0;
 
-    ref.current.addEventListener("wheel", onWheel, { passive: false });
-  };
+    for (let i = 0; i < gridItems.current.length; i++) {
+      const current = gridItems.current[i];
 
-  const createGrid = () => {
+      current.target -= deltaWheelY * current.scrollSpeed;
+      current.target = Math.round(current.target * 1000) / 1000;
+    }
+  }, []);
+
+  const applyConstrainsPerThumb = useCallback((item: SectionPhotoGridItem) => {
+    const currentY = item.y + item.yPos;
+
+    const offsetSpeedY = totalHeight.current * item.scrollSpeed - totalHeight.current;
+    const totalHeightOffsetSpeed = totalHeight.current + offsetSpeedY;
+
+    if (currentY < -totalHeightOffsetSpeed + windowHeight) {
+      // you want to always add the same delta to avoid discrepancy between target values!
+      // so we use the target to calculate this since we are going to update it
+      const deltaDiff = totalHeightOffsetSpeed + item.target;
+      item.target = 0 + deltaDiff;
+      item.y = 0 + deltaDiff - item.deltaY;
+    } else if (currentY >= totalHeightOffsetSpeed - item.height) {
+      // versus the target where the infinite loop starts: (this.totalHeightOffsetSpeed - item.height)
+      const deltaDiff = item.target + item.yPos - (totalHeightOffsetSpeed - item.height);
+      item.target = -item.yPos - item.height + deltaDiff;
+      item.y = -item.yPos - item.height + deltaDiff - item.deltaY;
+    }
+  }, [windowHeight]);
+
+  const draw = useCallback((gridElement: SectionPhotoGridItem) => {
+    const y = gridElement.yPos + gridElement.y;
+    const previousY = gridElement.yPos + gridElement.previousY;
+
+    // logic to avoid useless painting
+    const isGridElementBefore = y < -gridElement.height && previousY < -gridElement.height;
+    const isGridElementAfter = y > windowHeight && previousY > windowHeight;
+    if ((!isGridElementBefore && !isGridElementAfter) || !gridElement.hasBeenDrawn) {
+      gridElement.ref.style.transform = `translate3d(${gridElement.xPos}px, ${y}px, 0px)`;
+    }
+
+    if (!gridElement.hasBeenDrawn) {
+      gridElement.hasBeenDrawn = true;
+    }
+  }, [windowHeight]);
+
+  const update = useCallback(() => {
+    for (let i = 0; i < gridItems.current.length; i++) {
+      const current = gridItems.current[i];
+      current.previousY = current.y;
+
+      // we also need to make sure to add current.scrollSpeed here
+      current.target += AUTO_SPEED * lerpedAutoScroll.current * current.scrollSpeed;
+      current.y += AUTO_SPEED * lerpedAutoScroll.current * current.scrollSpeed;
+
+      current.deltaY = current.target - current.y;
+      current.y = current.y + current.deltaY * current.friction;
+
+      current.y = Math.round(1000 * current.y) / 1000;
+      applyConstrainsPerThumb(current);
+
+      draw(current);
+    }
+
+    // turn the autoScroll.current on/off
+    if (autoScroll.current === 1 && timeAutoScroll.current <= 1) {
+      timeAutoScroll.current += 0.01;
+      if (Math.round(1000 * lerpedAutoScroll.current) / 1000 === autoScroll.current) {
+        lerpedAutoScroll.current = autoScroll.current;
+      }
+      lerpedAutoScroll.current = lerp(lerpedAutoScroll.current, 1, easeInCubic(timeAutoScroll.current));
+    }
+
+    if (autoScroll.current === 0 && timeAutoScroll.current <= 1) {
+      timeAutoScroll.current += 0.01;
+      if (Math.round(1000 * lerpedAutoScroll.current) / 1000 === autoScroll.current) {
+        lerpedAutoScroll.current = autoScroll.current;
+      }
+      lerpedAutoScroll.current = lerp(lerpedAutoScroll.current, 0, easeOutCubic(timeAutoScroll.current));
+    }
+
+    if (target.current === oldTarget.current) {
+      fakeTimer.current++;
+    } else {
+      fakeTimer.current = 0;
+    }
+
+    if (fakeTimer.current > 30 && autoScroll.current !== 1) {
+      autoScroll.current = 1;
+      timeAutoScroll.current = 0;
+    }
+
+    oldTarget.current = target.current;
+    requestAnimationFrame.current = window.requestAnimationFrame(() => update());
+  }, [applyConstrainsPerThumb, draw]);
+
+  const reflow = useCallback(() => {
+    let furthestEnd = 0;
+    const halfWindowWidth = windowWidth / 2;
+    const paddingVertical = PADDING_VERTICAL * windowWidth;
+
+    for (let i = 0; i < gridItems.current.length; i++) {
+      const current = gridItems.current[i];
+
+      let columnWidth = halfWindowWidth / COLUMNS - PADDING_COLUMN;
+
+      // let's just make the wide image slightly bigger
+      const aspectRatio = current.width / current.height;
+      if (aspectRatio > 1) {
+        columnWidth *= 1.2;
+        columnWidth -= 0.1;
+      }
+
+      current.ref.style.width = `${columnWidth}px`;
+      current.width = columnWidth;
+
+      const height = current.ref.offsetHeight;
+      current.height = height;
+
+      const columnHeight = columnWidth / IMAGE_BASE_RATIO;
+      const yPosOriginal = paddingVertical / 2 + columnHeight * current.yColumn;
+
+      // we now need to offset based on scrollSpeed since we want to make sure item will reach middle of the screen at the same time
+      // to do this, we are calculating the difference between the calculateDistance from start to middle of the screen
+      // add offsetting this by the difference created by the speed
+      const distanceToCenter = yPosOriginal - (windowHeight / 2 - height / 2);
+      current.xPos = (halfWindowWidth / COLUMNS) * current.xColumn + PADDING_COLUMN / 2;
+      current.yPos = yPosOriginal + distanceToCenter * current.scrollSpeed - distanceToCenter;
+
+      const end = yPosOriginal + height;
+      if (end > furthestEnd) {
+        furthestEnd = end;
+      }
+    }
+    totalHeight.current = furthestEnd + paddingVertical / 2;
+
+    for (let i = 0; i < gridItems.current.length; i++) {
+      const current = gridItems.current[i];
+      current.y = totalHeight.current * targetPercent.current;
+      current.target = totalHeight.current * targetPercent.current;
+
+      // make sure to redraw
+      current.hasBeenDrawn = false;
+    }
+  }, [windowHeight, windowWidth]);
+
+  const createGrid = useCallback(() => {
     const DOMElements = ref.current && ref.current.querySelectorAll(".about-photo");
     const positions = createGridPositions(DOMElements.length);
 
@@ -168,7 +314,7 @@ export const SectionPhoto: FunctionComponent<SectionPhotoProps> = ({
     });
 
     reflow();
-  };
+  }, [reflow]);
 
   const createGridPositions = (itemsLength: number): SectionPhotoGridPosition[] => {
     let currentRow = -1;
@@ -191,199 +337,62 @@ export const SectionPhoto: FunctionComponent<SectionPhotoProps> = ({
   };
 
   const createGridItem = (element: HTMLElement, index: number, gridPositions: SectionPhotoGridPosition[]) => {
+    const friction = 0.1 + Math.random() * 0.1;
     const scrollSpeed = 0.8 + Math.random() * 0.5;
 
     return {
       ref: element,
       hasBeenDrawn: false,
-      previousY: 0,
       y: 0,
+      previousY: 0,
       xPos: 0,
       yPos: 0,
       xColumn: gridPositions[index].x,
       yColumn: gridPositions[index].y,
       index,
       target: 0,
-      friction: 0.1 + Math.random() * 0.1,
+      friction,
       deltaY: 0,
-      scrollSpeed: scrollSpeed,
+      scrollSpeed,
       width: element.offsetWidth,
       height: element.offsetHeight,
     };
   };
 
-  // main target Y here
-  const updateTarget = (evt: WheelEvent) => {
-    let deltaWheelY = 0;
-    if (evt.deltaY > 0) {
-      deltaWheelY = Math.min(evt.deltaY, maxWheel);
-    } else {
-      deltaWheelY = -Math.min(Math.abs(evt.deltaY), maxWheel);
-    }
+  const initScroll = useCallback(() => {
+    const onWheel = (event: WheelEvent) => {
+      event.preventDefault();
+      updateTarget(event);
+    };
 
-    target.current -= deltaWheelY;
-    target.current = Math.round(target.current * 10000) / 10000;
-    targetPercent.current = (target.current / totalHeight.current) % 1;
+    ref.current.addEventListener("wheel", onWheel, { passive: false });
+  }, [updateTarget]);
 
-    autoScroll = 0;
-    timeAutoScroll = 0;
+  // reflow on window resize
+  useEffect(() => {
+    reflow();
+  }, [windowWidth, windowHeight, reflow]);
 
-    for (let i = 0; i < gridItems.current.length; i++) {
-      const current = gridItems.current[i];
-
-      current.target -= deltaWheelY * current.scrollSpeed;
-      current.target = Math.round(current.target * 1000) / 1000;
-    }
-  };
-
-  const update = () => {
-    for (let i = 0; i < gridItems.current.length; i++) {
-      const current = gridItems.current[i];
-      current.previousY = current.y;
-
-      // we also need to make sure to add current.scrollSpeed here
-      current.target += autoSpeed * lerpedAutoScroll * current.scrollSpeed;
-      current.y += autoSpeed * lerpedAutoScroll * current.scrollSpeed;
-
-      current.deltaY = current.target - current.y;
-      current.y = current.y + current.deltaY * current.friction;
-
-      current.y = Math.round(1000 * current.y) / 1000;
-      applyConstrainsPerThumb(current);
-
-      draw(current);
-    }
-
-    // turn the autoScroll on/off
-    if (autoScroll === 1 && timeAutoScroll <= 1) {
-      timeAutoScroll += 0.01;
-      if (Math.round(1000 * lerpedAutoScroll) / 1000 === autoScroll) {
-        lerpedAutoScroll = autoScroll;
-      }
-      lerpedAutoScroll = lerp(lerpedAutoScroll, 1, easeInCubic(timeAutoScroll));
-    }
-
-    if (autoScroll === 0 && timeAutoScroll <= 1) {
-      timeAutoScroll += 0.01;
-      if (Math.round(1000 * lerpedAutoScroll) / 1000 === autoScroll) {
-        lerpedAutoScroll = autoScroll;
-      }
-      lerpedAutoScroll = lerp(lerpedAutoScroll, 0, easeOutCubic(timeAutoScroll));
-    }
-
-    if (target.current === oldTarget) {
-      fakeTimer++;
-    } else {
-      fakeTimer = 0;
-    }
-
-    if (fakeTimer > 30 && autoScroll !== 1) {
-      autoScroll = 1;
-      timeAutoScroll = 0;
-    }
-
-    oldTarget = target.current;
-    requestAnimationFrame = window.requestAnimationFrame(() => update());
-  };
-
-  const applyConstrainsPerThumb = (item: SectionPhotoGridItem) => {
-    const currentY = item.y + item.yPos;
-
-    const offsetSpeedY = totalHeight.current * item.scrollSpeed - totalHeight.current;
-    const totalHeightOffsetSpeed = totalHeight.current + offsetSpeedY;
-
-    if (currentY < -totalHeightOffsetSpeed + windowHeight) {
-      // you want to always add the same delta to avoid discrepancy between target values!
-      // so we use the target to calculate this since we are going to update it
-      const deltaDiff = totalHeightOffsetSpeed + item.target;
-      item.target = 0 + deltaDiff;
-      item.y = 0 + deltaDiff - item.deltaY;
-    } else if (currentY >= totalHeightOffsetSpeed - item.height) {
-      // versus the target where the infinite loop starts : (this.totalHeightOffsetSpeed - item.height)
-      const deltaDiff = item.target + item.yPos - (totalHeightOffsetSpeed - item.height);
-      item.target = -item.yPos - item.height + deltaDiff;
-      item.y = -item.yPos - item.height + deltaDiff - item.deltaY;
-    }
-  };
-
-  const draw = (gridElement: SectionPhotoGridItem) => {
-    const y = gridElement.yPos + gridElement.y;
-    const previousY = gridElement.yPos + gridElement.previousY;
-
-    // logic to avoid useless painting
-    const isGridElementBefore = y < -gridElement.height && previousY < -gridElement.height;
-    const isGridElementAfter = y > windowHeight && previousY > windowHeight;
-    if ((!isGridElementBefore && !isGridElementAfter) || !gridElement.hasBeenDrawn) {
-      gridElement.ref.style.transform = `translate3d(${gridElement.xPos}px,${y}px,0px)`;
-    }
-
-    if (!gridElement.hasBeenDrawn) {
-      gridElement.hasBeenDrawn = true;
-    }
-  };
-
-  const reflow = useCallback(() => {
-    const halfWindowWidth = windowWidth / 2;
-    const items = gridItems.current;
-    let furthestEnd = 0;
-    const paddingVertical = PADDING_VERTICAL * windowWidth;
-
-    for (let i = 0; i < items.length; i++) {
-      const current = items[i];
-
-      let columnWidth = halfWindowWidth / COLUMNS - PADDING_COLUMN;
-
-      // let's just make the wide image slightly bigger
-      const aspectRatio = current.width / current.height;
-      if (aspectRatio > 1) {
-        columnWidth *= 1.2;
-        columnWidth -= 0.1;
-      }
-
-      current.ref.style.width = `${columnWidth}px`;
-      current.width = columnWidth;
-
-      const height = current.ref.offsetHeight;
-      current.height = height;
-
-      const columnHeight = columnWidth / IMAGE_BASE_RATIO;
-      const yPosOriginal = paddingVertical / 2 + columnHeight * current.yColumn;
-
-      // we now need to offset based on scrollSpeed since we want to make sure item will reach middle of the screen at the same time
-      // to do this, we are calculating the difference between the calculateDistance from start to middle of the screen
-      // add offsetting this by the difference created by the speed
-      const distanceToCenter = yPosOriginal - (windowHeight / 2 - height / 2);
-      current.xPos = (halfWindowWidth / COLUMNS) * current.xColumn + PADDING_COLUMN / 2;
-      current.yPos = yPosOriginal + distanceToCenter * current.scrollSpeed - distanceToCenter;
-
-      const end = yPosOriginal + height;
-      if (end > furthestEnd) {
-        furthestEnd = end;
-      }
-    }
-    totalHeight.current = furthestEnd + paddingVertical / 2;
-
-    for (let i = 0; i < items.length; i++) {
-      const current = items[i];
-      current.y = totalHeight.current * targetPercent.current;
-      current.target = totalHeight.current * targetPercent.current;
-
-      // make sure to redraw
-      current.hasBeenDrawn = false;
-    }
-  }, [windowHeight, windowWidth]);
-
+  // init
   useEffect(() => {
     initScroll();
     createGrid();
     update();
 
-    return () => cancelAnimationFrame(requestAnimationFrame);
-  }, []);
+    return () => {
+      if (!requestAnimationFrame.current) return;
 
-  useEffect(() => {
-    reflow();
-  }, [windowWidth, windowHeight, reflow]);
+      cancelAnimationFrame(requestAnimationFrame.current);
+    };
+  }, [createGrid, initScroll, update]);
+
+  // get random background color
+  const backgroundColor = useMemo(() => {
+    const backgroundColors = ["red-700", "green-700", "blue-700"];
+    const index = getRandomInteger(0, backgroundColors.length - 1);
+
+    return backgroundColors[index];
+  }, []);
 
   return (
     <Flex
